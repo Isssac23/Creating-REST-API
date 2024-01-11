d9d77c986ae8b4a1db989fbd429e18d364e93e6e
isssac23 <is-haaq.omar@hotmail.co.uk>
2024-01-11 00:02:32 +0000

    Creating a REST API to request data to

--- RESTAPI.py
+++ RESTAPI.py
@@ -1,6 +1,8 @@
+import json, requests
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+#The initial user data which is being inputted to the API
 users_list = [
     {
         "id": 0,
@@ -10,10 +12,12 @@ users_list = [
     },
 ]
 
+#Displaying default text on API
 @app.route("/")
 def index():
     return "Hi!"
 
+#Creating GET and POST request functions to be able to retreive and upload new users data
 @app.route('/users', methods=['GET', 'POST'])
 def users():
     if request.method == 'GET':
@@ -35,13 +39,18 @@ def users():
             'DOB' : new_DOB,
         }
         users_list.append(new_obj)
+        with open('data.json', 'w') as f:  #Function to retrieve and store data from API to json file while server is running
+            json.dump(users_list, f)
         return jsonify(users_list), 201
     
+#Creating GET, PUT, and DELETE request functions to be able to modify the data using the user id    
 @app.route('/user/<int:id>', methods= ['GET', 'PUT', 'DELETE'])
 def single_user(id):
     if request.method == 'GET':
         for user in users_list:
             if user['id'] == id:
+                with open('data.json', 'w') as f:
+                    json.dump(users_list, f)
                 return jsonify(user)
             pass
     if request.method == 'PUT':
@@ -56,12 +65,17 @@ def single_user(id):
                     'age': user['age'], 
                     'DOB': user['DOB']
                 }
+                with open('data.json', 'w') as f:
+                    json.dump(users_list, f)
                 return jsonify(updated_user)
     if request.method == 'DELETE':
         for index, user in enumerate(users_list):
             if user['id'] == id:
                 users_list.pop(index)
-                return jsonify(users_list)     
+                with open('data.json', 'w') as f:
+                    json.dump(users_list, f)
+                return jsonify(users_list)    
+
 
 if __name__ == '__main__':
     app.run()
